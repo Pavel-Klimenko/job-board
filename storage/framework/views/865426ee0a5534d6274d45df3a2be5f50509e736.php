db2e7757d2abbc995422c961f0157b4b8eb78d95
Pavel <pavel.klimenko.1989@gmail.com>
2022-02-05 14:07:42 +0300

    deleted storage path

--- storage/framework/views/865426ee0a5534d6274d45df3a2be5f50509e736.php
+++
@@ -1,6 +0,0 @@
-<p>
-    <?php echo e($email); ?> <br>
-    <?php echo e($phone); ?> <br>
-    <?php echo e($address); ?>
-
-</p><?php /**PATH /var/www/job-board/resources/views/components/contact-info.blade.php ENDPATH**/ ?>
